ea57c302792b99d0203db8187a3c63e70a2ea608
kyepi <kkh3972460@naver.com>
2021-07-14 20:05:07 +0900

    Create 백준 1152번.c

---
+++ 백준 1152번.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	int count = 1;
+	char str[1000001];
+	scanf("%[^\n]s", str);
+
+
+	for (int i = 0; i < strlen(str); i++)
+	{
+		if (str[i] == ' ')
+		{
+			count++;
+		}
+
+	}
+	if (str[0] == ' ')
+		count = count - 1;
+	if (str[strlen(str)-1] == ' ')
+		count = count - 1;`
+	printf("%d", count);
+
+}
